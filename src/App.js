8f2d42f98b02ee1e1cfccae33a872797c5ba0e15
MehdiShu <mehdidevelop3@gmail.com>
2021-12-16 10:43:19 +0330

    version one for todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { useState } from 'react';
+import data from './Data/data.json';
+import ToDoList from './Components/ToDoList';
+
+//components
+import Header from "./Components/Header";
+ 
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const [ toDoList, setToDoList ] = useState(data);
+ 
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id == id ? { ...task, complete: !task.complete } : { ...task};
+    });
+    setToDoList(mapped);
+  }
+ 
+ return (
+   <div className="App">
+     <Header />
+     <ToDoList toDoList={toDoList}/>
+   </div>
+ );
 }
-
+ 
 export default App;
